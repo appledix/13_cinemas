73319e2fff192369f951288f5231943dc1f170f1
appledix <dxl2010@mail.ru>
2017-01-28 01:46:25 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
 # Cinemas
 
-[TODO. There will be project description]
+Данный скрипт выводит в консоль десять популярных фильмов с самым высоким рейтингом, показываемых на данных момент в кинотеатрах.
+Список фильмов берётся из расписания [афиши](http://www.afisha.ru/msk/schedule_cinema/), а рейтинг для сортировки - с [кинопоиска](https://www.kinopoisk.ru/).
+
+Во избежание бана кинопоиском (за чрезмерное кол-во обращений к сайту), установлена задержка в 10 секунд между запросами рейтингов каждого фильма. В связи с этим время работы скрипта оставляет желать лучшего.
+
+### Использование
+В терминале: `python3.5 cinemas.py`
+
+### Пример работы
+```
+ • Ла-Ла Ленд
+Rating: 8.54/10 (Total votes: 42 716)
+Number of cinemas: 109
+ • Невеста
+Rating: 7.66/10 (Total votes: 4078)
+Number of cinemas: 142
+ • Молчание
+Rating: 7.62/10 (Total votes: 1944)
+Number of cinemas: 90
+ • Пассажиры
+Rating: 7.15/10 (Total votes: 44 469)
+Number of cinemas: 46
+ • Рай
+Rating: 7.06/10 (Total votes: 1519)
+Number of cinemas: 66
+ • Почему он?
+Rating: 6.82/10 (Total votes: 9950)
+Number of cinemas: 111
+ • Иллюзия любви
+Rating: 6.71/10 (Total votes: 367)
+Number of cinemas: 27
+ • Притяжение
+Rating: 6.63/10 (Total votes: 4409)
+Number of cinemas: 164
+ • Монстр-траки
+Rating: 6.17/10 (Total votes: 1317)
+Number of cinemas: 50
+ • Кредо убийцы
+Rating: 6.16/10 (Total votes: 33 447)
+Number of cinemas: 95
+```
+
+### Установка скрипта 
+В терминале: `git clone https://github.com/appledix/13_cinemas.git`
+
+## Установка зависимостей
+В терминале: `pip3 install -r requirements.txt`
+
 
 # Project Goals
 
--- cinemas.py
+++ cinemas.py
@@ -1,20 +1,85 @@
+import heapq
+import unicodedata
+import time
+
+from bs4 import BeautifulSoup
+import requests
 
 
-def fetch_afisha_page():
-    pass
+def get_raw_html(url):
+    return requests.get(url).text
 
+def get_movies_info_from_afisha(afisha_html):
+    soup = BeautifulSoup(afisha_html, 'html.parser')
+    for div in soup.find_all('div', class_='m-disp-table'):
+        title = div.h3.a.string
+        schedule_html = div.find_next('table')
+        number_of_cinemas = len(schedule_html.find_all('td', class_='b-td-item'))
+        yield title, number_of_cinemas
 
-def parse_afisha_list(raw_html):
-    pass
+def get_movie_page_from_kinopoisk(movie_title):
+    kinopoisk_search_url = 'https://www.kinopoisk.ru/index.php'
+    payload = {'first': 'yes', 'kp_query': movie_title}
+    user_agent = ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) '
+                  'Gecko/20100101 Firefox/50.0')
+    headers = {'User-Agent':user_agent}
+    timeout = (5,5)
+    seconds_to_wait = 10
+    request = requests.get(url=kinopoisk_search_url,
+                           params=payload,
+                           timeout=timeout,
+                           headers=headers)
+    time.sleep(seconds_to_wait) # To avoid ban
+    return request.text
 
+def get_rating_info(movie_page_html):
+    soup = BeautifulSoup(movie_page_html, 'html.parser')
+    div = soup.find('div', id='block_rating')
+    if div:
+        average_rating = div.find('span', class_='rating_ball')
+        number_of_ratings = div.find('span', class_='ratingCount')
+        if average_rating and number_of_ratings:
+            number_of_ratings = unicodedata.normalize("NFKD", number_of_ratings.text)
+            return float(average_rating.text), number_of_ratings
+    return None, None
 
-def fetch_movie_info(movie_title):
-    pass
+def get_movies_info(num_of_movies=None, is_rated=False, is_mass_market=False):
+    movies_info = []
+    min_num_of_cinemas = 25 # Minimal number of cinemas for mass market movie
+    afisha_schedule = 'http://www.afisha.ru/msk/schedule_cinema/'
+    afisha_html = get_raw_html(afisha_schedule)
+    afisha_movies_info = get_movies_info_from_afisha(afisha_html)
 
+    for title, num_of_cinemas in afisha_movies_info:
+        if num_of_movies and (len(movies_info) >= num_of_movies):
+            break
+        movie_page = get_movie_page_from_kinopoisk(title)
+        rating, ratings_counter = get_rating_info(movie_page)
+        if (is_rated and (not rating)) \
+        or  (is_mass_market and (num_of_cinemas < min_num_of_cinemas)):
+            continue
+        movies_info.append({'title': title,
+                      'number of cinemas': num_of_cinemas,
+                      'rating': rating,
+                      'number of ratings': ratings_counter})
+    return movies_info
 
-def output_movies_to_console(movies):
-    pass
+def get_rating(movies_info):
+    rating = movies_info['rating']
+    return (rating if rating else 0.0)
 
+def main():
+    number_of_best_movies = 10
+    movies_info = get_movies_info(is_rated=True,
+                                  is_mass_market=True)
+    best_movies = heapq.nlargest(number_of_best_movies,
+                                movies_info,
+                                key=get_rating)
+    for movie in best_movies:
+        print(' • {}'.format(movie['title'])) 
+        print('Rating: {:.2f}/10 (Total votes: {})'.format(
+            movie['rating'], movie['number of ratings']))
+        print('Number of cinemas: {}'.format(movie['number of cinemas']))
 
 if __name__ == '__main__':
-    pass
+    main()
